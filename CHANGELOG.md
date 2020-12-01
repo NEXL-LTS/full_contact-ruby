9a1049d8657fcbd02a7bd6612f82f05b5f6f47c8
Grant Petersen-Speelman <grantspeelman@gmail.com>
2020-12-02 07:25:23 +1100

    add additional info to fake data

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+* add additional info to fake data
+
 ## [0.2.0]
 * add fake mode for when testing
 
--- spec/fc_enrich/fake_client_spec.rb
+++ spec/fc_enrich/fake_client_spec.rb
@@ -2,15 +2,15 @@ module FcEnrich
   RSpec.describe FakeClient do
     it 'works for /v3/person.enrich' do
       data = subject.post("/v3/person.enrich", { my_data: "true" })
-      expect(data).to include("ageRange" => "30-39",
-                              "details" => {},
-                              "fullName" => "Bart Lorang",
-                              "gender" => "Male",
+      expect(data).to include("ageRange" => "30-39", "fullName" => "Bart Lorang",
+                              "gender" => "Male", "organization" => "V1.vc",
                               "linkedin" => "https://www.linkedin.com/in/bartlorang",
-                              "location" => "Denver, CO, United States",
-                              "organization" => "V1.vc",
                               "title" => "Co-Founder & Managing Director",
-                              "twitter" => "https://twitter.com/bartlorang")
+                              "twitter" => "https://twitter.com/bartlorang",
+                              "details" => {
+                                "locations" => [{ "city" => "Denver", "country" => "United States", "region" => "CO" }],
+                                "name" => { "family" => "Lorang", "given" => "Bart" }
+                              })
     end
   end
 end
